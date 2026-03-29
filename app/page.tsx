2bc555e88d352e5ab1d1a8cbb0e6c47bc54a6d3a
Subhendu-Kumar <subhendukumardutta330@gmail.com>
2026-03-29 19:56:46 +0530

    feat: initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,7 @@
-import { navItems } from "@/data";
-import Grid from "@/components/Grid";
-import Hero from "@/components/Hero";
-import Footer from "@/components/Footer";
-import Approach from "@/components/Approach";
-import Experience from "@/components/Experience";
-import AllProjectsNew from "@/components/AllProjectsNew";
-import { FloatingNav } from "@/components/ui/FloatingNav";
-import AllSkills from "@/components/AllSkills";
-
-// import AllProjects from "@/components/AllProjects";
-// import RecentProjects from "@/components/RecentProjects";
-
-export default function Home() {
+const Home = () => {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
-      <div className="max-w-7xl w-full">
-        <FloatingNav navItems={navItems} />
-        <Hero />
-        <Grid />
-        <Experience />
-        <AllSkills />
-        {/* <RecentProjects /> */}
-        {/* <AllProjects /> */}
-        <AllProjectsNew />
-        <Approach />
-        <Footer />
-      </div>
-    </main>
-  );
+    <div className="">Home</div>
+  )
 }
+
+export default Home
